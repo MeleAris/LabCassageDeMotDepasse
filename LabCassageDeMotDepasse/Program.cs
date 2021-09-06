0726e20aca4135f582cb8b3c36c4d814b390b03b
Aristide <amelesusu@gmail.com>
2021-09-06 15:33:58 +0000

    Ajoutez des fichiers projet.

---
+++ LabCassageDeMotDepasse/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LabCassageDeMotDepasse
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string ComputeHash(string input, HashAlgorithm algorithm)
+            {
+                Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+
+                return BitConverter.ToString(hashedBytes);
+            }
+
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("F:\\Projects\\LabCassageDeMotDepasse\\input.txt");
+                line = sr.ReadLine();
+                StreamWriter sw = new StreamWriter("F:\\Projects\\LabCassageDeMotDepasse\\output.txt");
+                while (line != null)
+                {
+                    string hPassword = ComputeHash(line, new SHA256CryptoServiceProvider());
+                    sw.WriteLine(hPassword);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Exécution terminée");
+            }
+        }
+    }
+}
